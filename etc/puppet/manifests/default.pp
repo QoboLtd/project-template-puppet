3674d81052fd25c82cfab01b6c28888c0d165831
Leonid Mamchenkov <l.mamchenkov@qobo.biz>
2015-04-09 15:14:16 +0300

    Added comments to default manifest

--- etc/puppet/manifests/default.pp
+++ etc/puppet/manifests/default.pp
@@ -1,5 +1,8 @@
+# Default manifest
+# 
+# This manifest will run by default, if no other is given.
+# Therefor you should probably NOT modify it directly. Instead,
+# create other manifests.  This will save you some hair pulling.
 node default {
-
 	notice("Running the default manifest. Don't expect much.")
-
 }
